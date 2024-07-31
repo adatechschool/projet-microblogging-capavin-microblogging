2b9b6f92cc9c4ebf7ca64be708f385fba5efbe24
Katshual <139873320+Katshual@users.noreply.github.com>
2024-07-31 12:48:54 +0200

    view de tous les posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,9 @@
 
     @foreach($posts as $post)
-    <h1> {{$post->title}}</h1>
+    <h1> {{$post->title}}</h1> <h2>de {{$post->user->name}}<h2>
     <img src={{$post->picture_url}} alt={{$post->title}}>
 <p>
+   
     {{$post->content}}
 </p>
 @endforeach
