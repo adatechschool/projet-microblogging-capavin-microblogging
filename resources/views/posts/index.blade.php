6998a8dfa64bc3a6e8fb6894509c5b9239e8969e
Vincent Prévost <vincentprevost@live.fr>
2024-08-19 12:03:14 +0200

    page d'accueuil

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,7 @@
 
     @foreach($posts as $post)
-    <h1> {{$post->title}}</h1>
+    {{-- h1 : le titre // h2 : post est relié à la table users et à sa colonne name --}}
+    <h1> {{$post->title}}</h1><h2>de {{ $post->user->name }}</h2>
     <img src={{$post->picture_url}} alt={{$post->title}}>
 <p>
     {{$post->content}}
